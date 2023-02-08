4dd09dd8e491f25c5b0700677cc70118efb160cd
Ryan Foerster <ryanfoerster@outlook.be>
2023-02-08 19:08:20 +0100

    first commit

---
+++ testJeuJavaGit/src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int choix = 0;
+        
+        System.out.println("Indiquez votre choix");
+        choix = new Scanner(System.in).nextInt();
+
+        
+        System.out.println(choix);
+    }
+}
